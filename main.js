17a2a72fdc595d4af611123df7e6797b1258f74e
Krystyna Malewski <kmalewski@ergointeractive.com>
2019-04-01 11:25:14 -0400

    clean up file structure

--- main.js
+++
@@ -1,8 +0,0 @@
-console.log('JS connected');
-
-window.onload = () => {
-  const landingImage = document.querySelector('#landing-image');
-  // landingImage.style.width = `${window.innerWidth}px`;
-  landingImage.style.height = `${window.innerHeight - 84}px`;
-  console.log(landingImage)
-}
