4914ee03d1cc1543aeb09311888e5761022b878a
Krystyna Malewski <kmalewski@ergointeractive.com>
2019-04-01 11:36:36 -0400

    Update mobile nav bar

--- website-2/js/main.js
+++ website-2/js/main.js
@@ -3,28 +3,35 @@ $(document).ready(function() {
 	console.log("loading")
 
 	// grab the initial top offset of the navigation 
-   	var stickyNavTop = $('.nav').offset().top;
+   var stickyNavTop = $('.nav').offset().top;
 
  	// our function that decides weather the navigation bar should have "fixed" css position or not.
-   	var stickyNav = function(){
-	    var scrollTop = $(window).scrollTop(); // our current vertical position from the top
+  var stickyNav = function(){
+	  var scrollTop = $(window).scrollTop(); // our current vertical position from the top
 	         
-	    // if we've scrolled more than the navigation, change its position to fixed to stick to top,
-	    // otherwise change it back to relative
-	    if (scrollTop > stickyNavTop) { 
-	        $('.nav').addClass('sticky');
-	        $('.nav-offset').addClass('nav-top-65');
-	        $('.nav-offset').removeClass('nav-top-0');
+    // if we've scrolled more than the navig∂ation, change its position to fixed to stick to top,
+    // otherwise change it back to relative
+    if (scrollTop > stickyNavTop) { 
+        $('.nav').addClass('sticky');
+        $('.nav-offset').addClass('nav-top-65');
+        $('.nav-offset').removeClass('nav-top-0');
 
 
-	    } else {
-	        $('.nav').removeClass('sticky'); 
-	        $('.nav-offset').removeClass('nav-top-65'); 
-	        $('.nav-offset').addClass('nav-top-0'); 
+    } else {
+        $('.nav').removeClass('sticky'); 
+        $('.nav-offset').removeClass('nav-top-65'); 
+        $('.nav-offset').addClass('nav-top-0'); 
 
 
-	    }
+    }
 	};
+
+	// Toggle mobile nav bar on click
+	$('#nav-toggle').on('click', function() {
+	  this.classList.toggle('active');
+	  $('.nav')[0].classList.toggle('active');
+	  $('.nav-list')[0].classList.toggle('active');
+	});
 
 		stickyNav();
 		// and run it again every time you scroll
